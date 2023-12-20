3d9c7fc0228f940807a3e4d6d7626d482f9b33ab
Rubí <rubi_salcedocas@outlook.es>
2023-12-20 03:01:54 -0700

    New

---
+++ src/Ejercicio_1.java
@@ -0,0 +1,17 @@
+
+public class Ejercicio_1 {
+    public static void main (String[] arg){
+
+        //Presentando a mi familia
+        String Mama;
+        String Papa;
+        String Hermana;
+        String Yo;
+
+        Mama = "Irma Castro Castro";
+        Papa = "Ricardo Salcedo Chavarin";
+        Hermana = "Maria de Jesus Salcedo Castro";
+        Yo = "Rubi Salcedo Castro";
+
+    } 
+}
